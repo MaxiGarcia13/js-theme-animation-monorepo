20252b69b100b1b5b15f135ed571749eed577422
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-22 00:12:28 +0200

    Add ESLint configuration to ignore specific directories including dist, .astro, node_modules, and .vscode.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,11 @@ import { eslintConfig } from '@maxigarcia/eslint-config';
 export default eslintConfig(
   {
     typescript: true,
+    ignores: [
+      '**/dist/**',
+      '**/.astro/**',
+      '**/node_modules/**',
+      '**/.vscode/**',
+    ],
   },
 );
