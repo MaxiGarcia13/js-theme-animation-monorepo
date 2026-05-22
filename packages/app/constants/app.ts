6852eabc8fc9c54d8596fb2c4eef6cbafd14ea04
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-22 12:15:24 +0200

    feat: Add GitHub links to demo cards for improved accessibility
    - Introduced GitHub repository links in the CircularRevealDemoCard and SweepRevealDemoCard components for easy access to source code.
    - Created a new constants file to store GitHub URLs, enhancing maintainability and consistency across components.
    - Updated the AnimationDemoCard to accept a GitHub URL prop, allowing for dynamic linking based on the specific animation demo.

---
+++ packages/app/constants/app.ts
@@ -0,0 +1,5 @@
+export const GITHUB_REPO_URL
+  = 'https://github.com/MaxiGarcia13/js-theme-animation-monorepo';
+
+export const GITHUB_JS_THEME_ANIMATION_URL
+  = `${GITHUB_REPO_URL}/tree/main/packages/js-theme-animation/`;
