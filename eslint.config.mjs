fa6e78c84f7b396e8935b65aa2cca7f308c5368e
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-22 13:04:17 +0200

    feat: Update ESLint configuration for improved maintainability
    - Refactored ESLint configuration to utilize a separate ignores array, enhancing readability and maintainability.
    - Updated settings for Tailwind CSS to specify the global configuration file path dynamically based on the root directory.
    - Removed redundant ignores from the main configuration, streamlining the ESLint setup.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,29 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { eslintConfig } from '@maxigarcia/eslint-config';
+
+const ignores = [
+  '**/dist/**',
+  '**/.astro/**',
+  '**/node_modules/**',
+  '**/.vscode/**',
+];
+
+const rootDir = dirname(fileURLToPath(import.meta.url));
 
 export default eslintConfig(
   {
     typescript: true,
-    ignores: [
-      '**/dist/**',
-      '**/.astro/**',
-      '**/node_modules/**',
-      '**/.vscode/**',
-    ],
+    tailwindcss: true,
+    ignores,
+  },
+  {
+    ignores,
+    settings: {
+      tailwindcss: {
+        config: join(rootDir, 'packages/app/src/styles/global.css'),
+      },
+    },
+
   },
 );
