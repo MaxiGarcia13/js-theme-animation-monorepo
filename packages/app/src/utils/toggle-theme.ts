0e68563dcfe6f62838693473bb3c9e3e02f62758
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-22 11:10:43 +0200

    feat: Implement toggleTheme utility function for dark mode support
    - Added a new utility function `toggleTheme` to manage theme switching between dark and light modes.
    - The function updates the document's class list and toggles visibility of theme icons based on the current theme state.

---
+++ packages/app/src/utils/toggle-theme.ts
@@ -0,0 +1,17 @@
+export function toggleTheme(button: HTMLButtonElement) {
+  const isDark = !document.documentElement.classList.contains('dark');
+
+  document.documentElement.classList.toggle('dark', isDark);
+
+  const moonIcon = button.querySelector<HTMLElement>(
+    '[data-theme-icon="moon"] svg',
+  );
+  const sunIcon = button.querySelector<HTMLElement>(
+    '[data-theme-icon="sun"] svg',
+  );
+
+  moonIcon?.classList.toggle('hidden', isDark);
+  sunIcon?.classList.toggle('block', isDark);
+  moonIcon?.classList.toggle('block', !isDark);
+  sunIcon?.classList.toggle('hidden', !isDark);
+}
