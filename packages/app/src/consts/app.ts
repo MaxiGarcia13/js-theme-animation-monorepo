1579ab4a39449b1ba45f04d9cad03205c17b6c92
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-24 16:24:14 +0200

    refactor: update GitHub URLs for view transitions
    - Renamed the constant for the GitHub view transitions URL to improve clarity.
    - Updated related URLs for circular reveal, sweep reveal, fall, and page turn animations to reflect the new view transitions structure.

--- packages/app/src/consts/app.ts
+++ packages/app/src/consts/app.ts
@@ -1,17 +1,17 @@
 export const GITHUB_REPO_URL
   = 'https://github.com/MaxiGarcia13/js-theme-animation-monorepo';
 
-export const GITHUB_JS_THEME_ANIMATION_URL
+export const GITHUB_VIEW_TRANSITIONS_URL
   = `${GITHUB_REPO_URL}/tree/main/packages/view-transitions`;
 
 export const GITHUB_CIRCULAR_REVEAL_URL
-  = `${GITHUB_JS_THEME_ANIMATION_URL}/src/circular-reveal`;
+  = `${GITHUB_VIEW_TRANSITIONS_URL}/src/circular-reveal`;
 
 export const GITHUB_SWEEP_REVEAL_URL
-  = `${GITHUB_JS_THEME_ANIMATION_URL}/src/sweep-reveal`;
+  = `${GITHUB_VIEW_TRANSITIONS_URL}/src/sweep-reveal`;
 
 export const GITHUB_FALL_URL
-  = `${GITHUB_JS_THEME_ANIMATION_URL}/src/fall`;
+  = `${GITHUB_VIEW_TRANSITIONS_URL}/src/fall`;
 
 export const GITHUB_PAGE_TURN_URL
-  = `${GITHUB_JS_THEME_ANIMATION_URL}/src/page-turn`;
+  = `${GITHUB_VIEW_TRANSITIONS_URL}/src/page-turn`;
